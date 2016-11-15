3a533c2e6c91473a4e7e36899db628bf91ce11a5
Kevin Remde <kevin@remde.net>
2016-11-15 14:56:46 -0500

    First commit

---
+++ WebHost.ps1
@@ -0,0 +1,51 @@
+﻿
+Configuration WebHost
+{
+    param 
+    ( 
+        # Target nodes to apply the configuration 
+        [string[]]$NodeName = 'localhost' 
+    ) 
+
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+
+    Node $NodeName
+    {
+        # Install the IIS role 
+        WindowsFeature IIS 
+        { 
+            Ensure          = "Present" 
+            Name            = "Web-Server" 
+        } 
+        # Install the ASP .NET 4.5 role 
+        WindowsFeature AspNet45 
+        { 
+            Ensure          = "Present" 
+            Name            = "Web-Asp-Net45" 
+        } 
+
+        WindowsFeature WebStaticContent
+        { 
+            Ensure          = "Present" 
+            Name            = "Web-Static-Content" 
+        } 
+
+        WindowsFeature WebStatCompression
+        { 
+            Ensure          = "Present" 
+            Name            = "Web-Stat-Compression"
+        } 
+
+        WindowsFeature WebDynCompression
+        { 
+            Ensure          = "Present" 
+            Name            = "Web-Dyn-Compression"
+        } 
+
+        WindowsFeature WebMgmtConsole
+        { 
+            Ensure          = "Present" 
+            Name            = "Web-Mgmt-Console"
+        }
+    }
+}
